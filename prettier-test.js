9e11d5f284742d7a7936219dbe3014c7855088c1
Diogo Ferreira <diogo.ferreira18@ua.pt>
2025-10-11 18:23:21 +0100

    test 3 with lefthook

--- prettier-test.js
+++ prettier-test.js
@@ -1,21 +1,20 @@
-function   badlyFormattedFunction(  param1,param2   ){
-    const obj={
-        key1:"value1",
-            key2:      "value2",
-        key3:   "value3"
-    }
+function badlyFormattedFunction(param1, param2) {
+  const obj = {
+    key1: "value1",
+    key2: "value2",
+    key3: "value3",
+  };
 
-const array=[1,2,    3,
-    4,5]
+  const array = [1, 2, 3, 4, 5];
 
-    if(param1){
-return obj  }
-    else {
-        return    array
-    }
+  if (param1) {
+    return obj;
+  } else {
+    return array;
+  }
 }
 
-const str   =     "This is a poorly formatted string"
-const result=badlyFormattedFunction(true,false)
+const str = "This is a poorly formatted string";
+const result = badlyFormattedFunction(true, false);
 
-export default   badlyFormattedFunction
+export default badlyFormattedFunction;
