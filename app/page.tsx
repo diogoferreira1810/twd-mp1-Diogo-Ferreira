4c9523bcdc0ea5e2ea6ab6838dc26e089e7e88d8
Diogo Ferreira <diogo.ferreira18@ua.pt>
2025-10-11 21:31:55 +0100

    test 6 to see lefthook working

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
-import { draftMode } from "next/headers";
+import Link from 'next/link';
+import { draftMode } from 'next/headers';
 
-import Date from "./date";
-import CoverImage from "./cover-image";
-import Avatar from "./avatar";
-import MoreStories from "./more-stories";
+import Date from './date';
+import CoverImage from './cover-image';
+import Avatar from './avatar';
+import MoreStories from './more-stories';
 
-import { getAllPosts } from "@/lib/api";
-import { CMS_NAME, CMS_URL } from "@/lib/constants";
+import { getAllPosts } from '@/lib/api';
+import { CMS_NAME, CMS_URL } from '@/lib/constants';
 
 function Intro() {
   return (
@@ -16,14 +16,14 @@ function Intro() {
         Blog de TDW.
       </h1>
       <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Teste{" "}
+        Teste{' '}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-success duration-200 transition-colors"
         >
           Next.js
-        </a>{" "}
-        e{" "}
+        </a>{' '}
+        e{' '}
         <a
           href={CMS_URL}
           className="underline hover:text-success duration-200 transition-colors"
