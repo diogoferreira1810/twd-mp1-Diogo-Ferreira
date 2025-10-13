d047daee0bc20cdebe1c481c2c59c35f2509fae8
Diogo Ferreira <diogo.ferreira18@ua.pt>
2025-10-13 12:22:19 +0100

    first steps in deploying in Netlify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
+    unoptimized: true,
     loader: 'custom',
     formats: ['image/avif', 'image/webp'],
   },
+  output: 'export',
 };
